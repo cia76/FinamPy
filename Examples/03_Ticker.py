caadd51c0ab179225c9c3a6df7b224197d7043f9
cia76 <ichechet@mail.ru>
2023-08-05 09:33:36 +0500

    Унификация оформления

--- Examples/03_Ticker.py
+++ Examples/03_Ticker.py
@@ -5,7 +5,7 @@ from FinamPy.Config import Config
 if __name__ == '__main__':  # Точка входа при запуске этого скрипта
     fp_provider = FinamPy(Config.AccessToken)  # Подключаемся
 
-    symbols = (('TQBR', 'SBER'), ('FUT', 'SiU3'), ('FUT', 'RIU3'))  # Кортеж тикеров в виде (код площадки, код тикера)
+    symbols = (('TQBR', 'SBER'), ('TQBR', 'VTBR'), ('FUT', 'SiU3'), ('FUT', 'RIU3'))  # Кортеж тикеров в виде (код площадки, код тикера)
 
     print('Получаем информацию обо всех тикерах (займет несколько секунд)...')
     securities = fp_provider.get_securities()  # Получаем информацию обо всех тикерах
@@ -14,7 +14,7 @@ if __name__ == '__main__':  # Точка входа при запуске это
         for board, code in symbols:  # Пробегаемся по всем тикерам
             try:  # Пытаемся найти тикер
                 si = next(item for item in securities.securities if item.board == board and item.code == code)
-                # print(si)
+                # print('Ответ от сервера:', si)
                 print(f'\nИнформация о тикере {si.board}.{si.code} ({si.short_name}, {fp_provider.markets[si.market]}):')
                 print(f'Валюта: {si.currency}')
                 print(f'Лот: {si.lot_size}')
