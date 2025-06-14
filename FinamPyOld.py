84e0eebfdf00809a62b8db59801b1c707a435b4b
cia76 <ichechet@mail.ru>
2025-06-14 10:46:36 +0500

    Задаем временнУю зону UTC если не задана

--- FinamPyOld.py
+++ FinamPyOld.py
@@ -666,6 +666,6 @@ class FinamPyOld:
         :param bool tzinfo: Отображать временнУю зону
         :return: Московское время
         """
-        dt_utc = utc.localize(dt)  # Задаем временнУю зону UTC
+        dt_utc = utc.localize(dt) if dt.tzinfo is None else dt  # Задаем временнУю зону UTC если не задана
         dt_msk = dt_utc.astimezone(self.tz_msk)  # Переводим в МСК
         return dt_msk if tzinfo else dt_msk.replace(tzinfo=None)
