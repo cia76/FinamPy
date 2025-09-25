f773abdb14930bead62d08de00a159a39a628721
cia76 <ichechet@mail.ru>
2025-09-25 16:44:07 +0500

    package_data

--- setup.py
+++ setup.py
@@ -13,6 +13,6 @@ setup(name='FinamPy',
             'googleapis-common-protos'  # Google API
       ],
       python_requires='>=3.12',
-      package_data={'FinamPy.grpc': ['*', '*/*']},  # Дополнительно копируем скрипты из папки grpc и вложенных в нее папок
+      package_data={'FinamPy.FinamPy.grpc': ['*', '*/*']},  # Дополнительно копируем скрипты из папки grpc и вложенных в нее папок
       include_package_data=True,  # Включаем дополнительные скрипты
       )
