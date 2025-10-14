af3a68518e5b172c73d01a5776f9cbdf3922789a
cia76 <ichechet@mail.ru>
2025-10-14 16:45:01 +0500

    Исправление ошибок

--- Examples/Ticker.py
+++ Examples/Ticker.py
@@ -2,7 +2,7 @@ import logging  # Выводим лог на консоль и в файл
 from datetime import datetime  # Дата и время
 
 from FinamPy import FinamPy
-from FinamPy.grpc.assets.assets_service_pb2 import AssetsRequest, AssetsResponse, GetAssetRequest, GetAssetResponse  # Информация по тикеру
+from FinamPy.grpc.assets.assets_service_pb2 import GetAssetRequest, GetAssetResponse  # Информация по тикеру
 
 
 if __name__ == '__main__':  # Точка входа при запуске этого скрипта
@@ -18,10 +18,11 @@ if __name__ == '__main__':  # Точка входа при запуске это
     datanames = ('TQBR.SBER', 'TQBR.HYDR', 'SPBFUT.SiZ5', 'SPBFUT.RIZ5', 'SPBFUT.BRZ5', 'SPBFUT.CNYRUBF')  # Кортеж тикеров
 
     for dataname in datanames:  # Пробегаемся по всем тикерам
-        finam_board, symbol = fp_provider.dataname_to_finam_board_ticker(dataname)
-        si: GetAssetResponse = fp_provider.call_function(fp_provider.assets_stub.GetAsset, GetAssetRequest(symbol=symbol, account_id=fp_provider.account_ids[0]))
+        finam_board, ticker = fp_provider.dataname_to_finam_board_ticker(dataname)  # Код режима торгов Финама и тикер
+        mic = fp_provider.get_mic(finam_board, ticker)  # Биржа тикера
+        si: GetAssetResponse = fp_provider.call_function(fp_provider.assets_stub.GetAsset, GetAssetRequest(symbol=f'{ticker}@{mic}', account_id=fp_provider.account_ids[0]))
         logger.info(f'Информация о тикере {si.board}.{si.ticker} ({si.name}, {si.type}) на бирже {si.mic}')
         logger.info(f'- Лот: {int(float(si.lot_size.value))}')
-        logger.info(f'- Шаг цены: {si.min_step / (10 * si.decimals)}')
+        logger.info(f'- Шаг цены: {si.min_step / (10 ** si.decimals)}')
         logger.info(f'- Кол-во десятичных знаков: {si.decimals}')
     fp_provider.close_channel()  # Закрываем канал перед выходом
