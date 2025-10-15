0f67621b698525f230d29f5357c0ab8580e65b1e
cia76 <ichechet@mail.ru>
2025-10-15 11:41:55 +0500

    Оформление

--- Examples/Accounts.py
+++ Examples/Accounts.py
@@ -24,7 +24,7 @@ if __name__ == '__main__':  # Точка входа при запуске это
 
         for position in account.positions:  # Пробегаемся по всем позициям
             si: GetAssetResponse = fp_provider.call_function(fp_provider.assets_stub.GetAsset, GetAssetRequest(symbol=position.symbol, account_id=fp_provider.account_ids[0]))
-            logger.info(f'- Позиция {position.symbol} ({si.name}) {int(float(position.quantity.value))} @ {float(position.average_price.value)} / {float(position.current_price.value)}')
+            logger.info(f'- Позиция {si.board}.{si.ticker} ({si.name}) {int(float(position.quantity.value))} @ {float(position.average_price.value)} / {float(position.current_price.value)}')
 
         logger.info('- Свободные средства:')
         for cash in account.cash:
@@ -35,9 +35,10 @@ if __name__ == '__main__':  # Точка входа при запуске это
         orders: OrdersResponse = fp_provider.call_function(fp_provider.orders_stub.GetOrders, OrdersRequest(account_id=account_id))  # Получаем заявки
         for order in orders.orders:  # Пробегаемся по всем заявкам
             if order.status == ORDER_STATUS_NEW:  # Если заявка еще не исполнилась
+                si: GetAssetResponse = fp_provider.call_function(fp_provider.assets_stub.GetAsset, GetAssetRequest(symbol=order.order.symbol, account_id=fp_provider.account_ids[0]))
                 order_type = 'Заявка' if order.order.type == ORDER_TYPE_LIMIT else 'Стоп заявка'
                 price = float(order.order.limit_price.value) if order.order.type == ORDER_TYPE_LIMIT else float(order.order.stop_price)  # Цена для лимитной и стоп заявок
                 order_side = "Покупка" if order.order.side.buy_sell == SIDE_BUY else "Продажа"
-                logger.info(f'- {order_type} заявка {order.order_id} {order_side} {order.order.symbol} {order.order.quantity} @ {price}')
+                logger.info(f'- {order_type} номер {order.order_id} {order_side} {si.board}.{si.ticker} ({si.name}) {order.order.quantity} @ {price}')
 
     fp_provider.close_channel()  # Закрываем канал перед выходом
