19387d04e16ebc88ecb59100d075554f9a069e1f
cia76 <ichechet@mail.ru>
2026-02-13 19:41:36 +0500

    Фьючерсный контракт на дату

--- Examples/Ticker.py
+++ Examples/Ticker.py
@@ -1,7 +1,15 @@
 import logging  # Выводим лог на консоль и в файл
-from datetime import datetime  # Дата и время
+from datetime import date, timedelta, datetime  # Дата и время
 
 from FinamPy import FinamPy
+
+
+def get_future_on_date(base, future_date=date.today()):  # Фьючерсный контракт на дату
+    if future_date.day > 15 and future_date.month in (3, 6, 9, 12):  # Если нужно переходить на следующий фьючерс
+        future_date += timedelta(days=30)  # то добавляем месяц к дате
+    period = 'H' if future_date.month <= 3 else 'M' if future_date.month <= 6 else 'U' if future_date.month <= 9 else 'Z'  # Месяц экспирации: 3-H, 6-M, 9-U, 12-Z
+    digit = future_date.year % 10  # Последняя цифра года
+    return f'SPBFUT.{base}{period}{digit}'
 
 
 if __name__ == '__main__':  # Точка входа при запуске этого скрипта
@@ -14,7 +22,7 @@ if __name__ == '__main__':  # Точка входа при запуске это
                         handlers=[logging.FileHandler('Ticker.log', encoding='utf-8'), logging.StreamHandler()])  # Лог записываем в файл и выводим на консоль
     logging.Formatter.converter = lambda *args: datetime.now(tz=fp_provider.tz_msk).timetuple()  # В логе время указываем по МСК
 
-    datanames = ('TQBR.SBER', 'TQBR.HYDR', 'SPBFUT.SiZ5', 'SPBFUT.RIZ5', 'SPBFUT.BRZ5', 'SPBFUT.CNYRUBF')  # Кортеж тикеров
+    datanames = ('TQBR.SBER', 'TQBR.HYDR', get_future_on_date("Si"), get_future_on_date("RI"), 'SPBFUT.CNYRUBF', 'SPBFUT.IMOEXF')  # Кортеж тикеров
 
     for dataname in datanames:  # Пробегаемся по всем тикерам
         finam_board, ticker = fp_provider.dataname_to_finam_board_ticker(dataname)  # Код режима торгов Финама и тикер
