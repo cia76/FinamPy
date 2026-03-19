836916343f5846f07fd7d9ad8b2c74cb8b83ff0f
cia76 <ichechet@mail.ru>
2026-03-19 16:46:29 +0500

    Версия API 2.13.0

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='FinamPy',
-      version='2026.02.03',  # <Год>.<Месяц>.<Номер в месяце>
+      version='2026.03.01',  # <Год>.<Месяц>.<Номер в месяце>
       author='Чечет Игорь Александрович',
       description='Библиотека-обертка, которая позволяет работать с Finam Trade API брокера Финам из Python',
       url='https://github.com/cia76/FinamPy',
@@ -14,6 +14,6 @@ setup(name='FinamPy',
             'types-protobuf',  # Timestamp
       ],
       python_requires='>=3.12',
-      package_data={'FinamPy': ['grpc/**/*']},  # Дополнительно копируем скрипты из папки grpc и вложенных в нее папок
+      package_data={'FinamPy': ['grpc/*']},  # Дополнительно копируем скрипты из папки grpc и вложенных в нее папок
       include_package_data=True,  # Включаем дополнительные скрипты
       )
