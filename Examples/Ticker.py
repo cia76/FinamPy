88a5d26b8fbc3f8ed55f49a4544faf1facbef45d
cia76 <ichechet@mail.ru>
2026-03-22 17:11:15 +0500

    Оформление

--- Examples/Ticker.py
+++ Examples/Ticker.py
@@ -22,7 +22,7 @@ if __name__ == '__main__':  # Точка входа при запуске это
                         handlers=[logging.FileHandler('Ticker.log', encoding='utf-8'), logging.StreamHandler()])  # Лог записываем в файл и выводим на консоль
     logging.Formatter.converter = lambda *args: datetime.now(tz=fp_provider.tz_msk).timetuple()  # В логе время указываем по МСК
 
-    datanames = ('TQBR.SBER', 'TQBR.HYDR', get_future_on_date("Si"), get_future_on_date("RI"), 'SPBFUT.CNYRUBF', 'SPBFUT.IMOEXF')  # Кортеж тикеров
+    datanames = ('TQBR.SBER', 'TQBR.HYDR', get_future_on_date('Si'), get_future_on_date('RI'), 'SPBFUT.CNYRUBF', 'SPBFUT.IMOEXF')  # Кортеж тикеров
 
     for dataname in datanames:  # Пробегаемся по всем тикерам
         finam_board, ticker = fp_provider.dataname_to_finam_board_ticker(dataname)  # Код режима торгов Финама и тикер
